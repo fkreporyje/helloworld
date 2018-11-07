36147f25a51f355f9dcc4f2bdcdc353ddfaac042
janrejzek <rejzek.jan@gmail.com>
2018-11-07 22:21:20 +0630

    LearningRejzaImpl

---
+++ src/main/java/cz/fkreporyje/helloworld/services/impl/LearningRejzaImpl.java
@@ -0,0 +1,16 @@
+package cz.fkreporyje.helloworld.services.impl;
+
+import cz.fkreporyje.helloworld.services.Learning;
+
+public class LearningRejzaImpl implements Learning {
+
+    @Override
+    public String getFirstTwoLetters(String input)  {
+        return null;
+    }
+    @Override
+    public String getLastTwoLetters(String input)  {
+        return null;
+    }
+
+}
