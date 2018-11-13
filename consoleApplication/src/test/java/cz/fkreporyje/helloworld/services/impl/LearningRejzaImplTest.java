8e6071e3700bd00b1896c20ec4da72163e314651
janrejzek <rejzek.jan@gmail.com>
2018-11-13 21:04:48 +0630

    test Rejža

---
+++ consoleApplication/src/test/java/cz/fkreporyje/helloworld/services/impl/LearningRejzaImplTest.java
@@ -0,0 +1,16 @@
+package cz.fkreporyje.helloworld.services.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LearningRejzaImplTest {
+
+    @Test
+    public void getFirstTwoLetters() {
+    }
+
+    @Test
+    public void getLastTwoLetters() {
+    }
+}
